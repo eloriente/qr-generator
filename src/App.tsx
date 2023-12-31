d0cec672520d73439d7ae4ff2c7881dd050ab6fb
eladioltb <eladioloriente@gmail.com>
2023-12-31 02:11:04 +0100

    feat: Base styles added

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <>Hello World</>
+        element: <h1>Hello World</h1>
       }
     ]
   }
--- src/components/ui-general/ui-styles/ui-styles.component.styled.ts
+++ src/components/ui-general/ui-styles/ui-styles.component.styled.ts
@@ -4,12 +4,23 @@ import { createGlobalStyle } from "styled-components";
 import FontsComponent from "../ui-fonts/ui-fonts.component.styled";
 
 const GlobalStyles = createGlobalStyle`
-  ${ FontsComponent }
-  
   * {
     box-sizing: border-box;
     font-family: ${({ theme }) => theme.fontFamily.fontFamilyA};
   }
+
+  body {
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.backgroundColorA};
+    display: flex;
+    flex-direction: column;
+    height: 100svh;
+    justify-content: center;
+    margin: 0;
+    width: 100vw;
+  }
+
+  ${ FontsComponent }
 `;
 
 export default GlobalStyles;
